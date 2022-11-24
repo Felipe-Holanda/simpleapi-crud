f68b8b08b2cf369c66f82f9db8e5b3a1f64345df
Felipe Holanda <felipe.nether@icloud.com>
2022-11-24 11:06:23 -0300

    Started working

--- src/app.js
+++ src/app.js
@@ -1,7 +1,22 @@
 import express from "express";
-
+import users from "./database";
+import { hash, compare } from "bcryptjs";
+import { v4 as uuidv4 } from "uuid";
+import 'dotenv/config';
 
 const app = express()
+app.use(express.json())
 
 
+//Middlewares
+
+//Services
+
+
+//Controllers
+
+
+//Routes
+
+app.listen(4001, () => console.log(`Servidor está executando na porta 4001`))
 export default app
